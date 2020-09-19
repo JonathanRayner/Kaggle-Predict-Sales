0df31d831d70bcd7f3a21cbbd545b3cd0e01a619
Jonathan Rayner <jonathan.j.rayner@gmail.com>
2020-09-19 11:29:20 +0200

    added python script for data cleaning

---
+++ clean_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+# summary of cleaning:
+#
+# small number of duplicates, remove these
+# remove the single item with negative price
+# remove the single large outlier item price (corporate software purchase)
+
+df = pd.read_csv("sales_train.csv")
+
+df.drop_duplicates(inplace=True)
+
+df = df[(df['item_price'] > 0) & (df['item_price'] < 6e4)]
+
+# save
+df.to_csv('train_clean.csv')
