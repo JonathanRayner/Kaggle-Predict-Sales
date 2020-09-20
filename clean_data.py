4fcd8f558121cecc63b5c8ef599d60782f59bf0a
Jonathan Rayner <jonathan.j.rayner@gmail.com>
2020-09-20 04:08:39 +0200

    final comments, scatter plots

--- clean_data.py
+++ clean_data.py
@@ -6,12 +6,15 @@ import numpy as np
 # small number of duplicates, remove these
 # remove the single item with negative price
 # remove the single large outlier item price (corporate software purchase)
+# remove the single large outlier item sales (very different price and counts from other times it was sold)
 
 df = pd.read_csv("sales_train.csv")
 
 df.drop_duplicates(inplace=True)
 
 df = df[(df['item_price'] > 0) & (df['item_price'] < 6e4)]
+
+df = df[df['item_cnt_day'] < 1500]
 
 # save
 df.to_csv('train_clean.csv', index=False)
